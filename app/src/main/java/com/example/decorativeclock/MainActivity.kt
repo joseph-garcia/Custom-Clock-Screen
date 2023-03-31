2ec1a92e2516543f67056015444813749c2d4a87
Joseph Garcia <robhaywood12@gmail.com>
2023-03-30 22:51:48 -0500

    remove screen rotate, add tap rotate clock

--- app/src/main/java/com/example/decorativeclock/MainActivity.kt
+++ app/src/main/java/com/example/decorativeclock/MainActivity.kt
@@ -4,8 +4,10 @@ package com.example.decorativeclock
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.graphics.ImageDecoder
 import android.graphics.drawable.BitmapDrawable
@@ -18,33 +20,60 @@ import android.util.Log
 import android.view.*
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.decorativeclock.SettingsActivity.Companion.IMAGE_PICK_REQUEST_CODE
+import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.math.max
 import kotlin.math.min
+import com.yalantis.ucrop.UCrop
+import com.yalantis.ucrop.model.AspectRatio
+import android.graphics.Point
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+
 
 class MainActivity : AppCompatActivity() {
 
+    // Initialize clockTextView
     private lateinit var clockTextView: TextView
+
+
+    private lateinit var gestureDetector: GestureDetector
+
+    // Initialize scaleGestureDetector for pinch-to-resize
     private lateinit var scaleGestureDetector: ScaleGestureDetector
     private var scaleFactor = 1f
-    // Add a new instance variable to store the last MotionEvent
-    private var lastEvent: MotionEvent? = null
 
-    private lateinit var resetPositionButton: Button
+    // Initialize variables for dragging the clock
     private var dX = 0f
     private var dY = 0f
 
+    // Initialize optionsIcon and fadeHandler for fading in/out the options icon
     private lateinit var optionsIcon: ImageView
     private val fadeHandler = Handler(Looper.getMainLooper())
     private var statusBarHeight = 0
-
     private val fadeOutHandler = Handler(Looper.getMainLooper())
-    private val fadeOutRunnable = Runnable { fadeOutViews() }
 
+    // Initialize notificationBarHandler for fading in/out the notification bar
     private val notificationBarHandler = Handler(Looper.getMainLooper())
 
+    private lateinit var saveBackgroundImageUri: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
+    companion object {
+        private const val REQUEST_IMAGE_PICK = 1000
+        private const val BACKGROUND_IMAGE_URI_KEY = "background_image_uri"
+    }
+
+    private lateinit var sharedPreferences: SharedPreferences
 
 
     @SuppressLint("ClickableViewAccessibility")
@@ -52,21 +81,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sharedPreferences = getSharedPreferences("decorative_clock_preferences", MODE_PRIVATE)
+
         // Initialize optionsIcon
         optionsIcon = findViewById(R.id.optionsIcon)
         optionsIcon.y = statusBarHeight.toFloat()
 
+        // Get the status bar height for use in the fade in/out behavior
         statusBarHeight = getStatusBarHeight()
 
+        // Listen for options icon click
         optionsIcon.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
 
+        // Listen for options icon long click
         val fadeOutRunnable = Runnable {
             fadeOutViews()
         }
 
+        // Add this line to cancel the fade out timer when the notification bar is tapped
         val frameLayout = findViewById<FrameLayout>(R.id.frameLayout)
         frameLayout.setOnTouchListener { view, event ->
             if (event.action == MotionEvent.ACTION_DOWN) {
@@ -84,9 +119,10 @@ class MainActivity : AppCompatActivity() {
 
 
         // Set up the fade in/out behavior
-        val decorView = window.decorView
-        decorView.setOnSystemUiVisibilityChangeListener { visibility ->
-            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+        val contentView = findViewById<View>(android.R.id.content)
+        val controller = WindowCompat.getInsetsController(window, contentView)
+        controller?.addOnControllableInsetsChangedListener { _, mask ->
+            if (mask and WindowInsetsCompat.Type.statusBars() == 0) {
                 fadeInViews()
             } else {
                 fadeOutViews()
@@ -109,6 +145,14 @@ class MainActivity : AppCompatActivity() {
             }
         }
         handler.post(updateClockRunnable)
+
+        // Gesture detector for rotating the clock
+        gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onDoubleTap(e: MotionEvent): Boolean {
+                rotateClockTextView()
+                return true
+            }
+        })
 
         // Load and set the saved clock data (position and scale factor)
         val clockData = loadClockPosition()
@@ -137,6 +181,7 @@ class MainActivity : AppCompatActivity() {
 
         clockTextView.setOnTouchListener { view, event ->
             scaleGestureDetector.onTouchEvent(event)
+            gestureDetector.onTouchEvent(event)
 
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
@@ -166,9 +211,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun rotateClockTextView() {
+        clockTextView.animate()
+            .rotationBy(90f)
+            .setDuration(500)
+            .start()
+    }
+
     override fun onResume() {
         super.onResume()
-        setBackgroundImage()
+        val backgroundImageUriString = sharedPreferences.getString(BACKGROUND_IMAGE_URI_KEY, null)
+        if (backgroundImageUriString != null) {
+            val backgroundImageUri = Uri.parse(backgroundImageUriString)
+            setBackgroundImage(backgroundImageUri)
+        }
     }
 
     // Add these helper methods to fade in and fade out the views
@@ -198,23 +254,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hideSystemUI() {
-        val decorView = window.decorView
-        val uiOptions = (View.SYSTEM_UI_FLAG_LOW_PROFILE
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
-        decorView.systemUiVisibility = uiOptions
+        val contentView = findViewById<View>(android.R.id.content)
+        val controller = WindowCompat.getInsetsController(window, contentView)
+        controller?.hide(WindowInsetsCompat.Type.statusBars())
     }
 
+
     private fun showSystemUI() {
-        val decorView = window.decorView
-        val uiOptions = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
-        decorView.systemUiVisibility = uiOptions
+        val contentView = findViewById<View>(android.R.id.content)
+        val controller = WindowCompat.getInsetsController(window, contentView)
+        controller?.show(WindowInsetsCompat.Type.statusBars())
     }
+
 
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
@@ -295,36 +346,63 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setBackgroundImage() {
-        val sharedPreferences = getSharedPreferences("clock_data", Context.MODE_PRIVATE)
-        val backgroundImageUriString = sharedPreferences.getString("background_image_uri", null)
+    private fun setBackgroundImage(resultUri: Uri) {
+        val backgroundImageView = findViewById<ImageView>(R.id.background_image)
 
-        if (backgroundImageUriString != null) {
-            val backgroundImageUri = Uri.parse(backgroundImageUriString)
-            val frameLayout = findViewById<FrameLayout>(R.id.frameLayout)
+        val requestOptions = RequestOptions()
+            .centerCrop()
+            .error(com.bumptech.glide.R.drawable.abc_control_background_material) // Replace with your own error image
 
-            try {
-                val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    ImageDecoder.decodeBitmap(ImageDecoder.createSource(contentResolver, backgroundImageUri))
-                } else {
-                    @Suppress("DEPRECATION")
-                    MediaStore.Images.Media.getBitmap(contentResolver, backgroundImageUri)
-                }
-                frameLayout.background = BitmapDrawable(resources, bitmap)
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Toast.makeText(this, "Error loading background image", Toast.LENGTH_SHORT).show()
-            }
-        }
+
+        Glide.with(this)
+            .load(resultUri)
+            .apply(requestOptions)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(backgroundImageView)
     }
+
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        setBackgroundImage()
+        val backgroundImageUriString = sharedPreferences.getString(BACKGROUND_IMAGE_URI_KEY, null)
+        if (backgroundImageUriString != null) {
+            val backgroundImageUri = Uri.parse(backgroundImageUriString)
+            setBackgroundImage(backgroundImageUri)
+        }
     }
 
 
+    private fun startImagePicker() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, IMAGE_PICK_REQUEST_CODE)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK && data != null) {
+            val selectedImageUri = data.data
+            if (selectedImageUri != null) {
+                val display = windowManager.defaultDisplay
+                val size = Point()
+                display.getSize(size)
+                val screenWidth = size.x
+                val screenHeight = size.y
+
+                val uCrop = UCrop.of(selectedImageUri, Uri.fromFile(File(cacheDir, "background_image.jpg")))
+                    .withAspectRatio(screenWidth.toFloat(), screenHeight.toFloat())
+                uCrop.start(this)
+            }
+        } else if (requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK) {
+            val resultUri = UCrop.getOutput(data!!)
+            if (resultUri != null) {
+                setBackgroundImage(resultUri)
+                editor.putString("background_image_uri", resultUri.toString()).apply()
+            }
+        }
+    }
 
 
 }
--- app/src/main/java/com/example/decorativeclock/SettingsActivity.kt
+++ app/src/main/java/com/example/decorativeclock/SettingsActivity.kt
@@ -2,14 +2,17 @@ package com.example.decorativeclock
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Point
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import com.yalantis.ucrop.UCrop
 import java.io.File
 import java.io.FileOutputStream
 
@@ -28,10 +31,10 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     private fun openImagePicker() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         startActivityForResult(intent, IMAGE_PICK_REQUEST_CODE)
     }
+
 
     companion object {
         const val IMAGE_PICK_REQUEST_CODE = 1001
@@ -43,34 +46,38 @@ class SettingsActivity : AppCompatActivity() {
         if (requestCode == IMAGE_PICK_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
             val selectedImageUri = data.data
             if (selectedImageUri != null) {
-                saveSelectedImageUri(selectedImageUri)
+                val display = windowManager.defaultDisplay
+                val size = Point()
+                display.getSize(size)
+                val screenWidth = size.x
+                val screenHeight = size.y
+
+                val uCrop = UCrop.of(selectedImageUri, Uri.fromFile(File(cacheDir, "background_image.jpg")))
+                    .withAspectRatio(screenWidth.toFloat(), screenHeight.toFloat())
+                uCrop.start(this)
+            }
+        } else if (requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK) {
+            val resultUri = UCrop.getOutput(data!!)
+            if (resultUri != null) {
+                saveCroppedImageUri(resultUri)
             }
         }
     }
 
-    private fun saveSelectedImageUri(uri: Uri) {
-        val inputStream = contentResolver.openInputStream(uri)
-        val directory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val file = File(directory, "background_image.jpg")
-
-        try {
-            inputStream.use { input ->
-                FileOutputStream(file).use { output ->
-                    input?.copyTo(output)
-                }
-            }
-
-            val sharedPreferences = getSharedPreferences("clock_data", Context.MODE_PRIVATE)
-            val editor = sharedPreferences.edit()
-            val fileUri = FileProvider.getUriForFile(this, applicationContext.packageName + ".fileprovider", file)
-            editor.putString("background_image_uri", fileUri.toString())
-            editor.apply()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Toast.makeText(this, "Error saving background image", Toast.LENGTH_SHORT).show()
-            Log.d("josephDebug", "Error saving background image: ${e.message}")
-        }
+    private fun saveCroppedImageUri(uri: Uri) {
+        val sharedPreferences = getSharedPreferences("decorative_clock_preferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("background_image_uri", uri.toString())
+        editor.apply()
     }
+
+
+    private fun isGifFile(context: Context, uri: Uri): Boolean {
+        val mimeType: String? = context.contentResolver.getType(uri)
+        return mimeType?.equals("image/gif", ignoreCase = true) == true
+    }
+
+
 
 
 }
